8fb7761b8ac9769ba124e90cad7785ae23be9994
chickenc <solutw@gmail.com>
2023-07-03 18:22:33 +0900

    first commit springPractice

---
+++ src/main/java/com/example/practice/MvcController1.java
@@ -0,0 +1,42 @@
+package com.example.practice;
+
+import org.springframework.cglib.core.ClassGenerator;
+import org.springframework.web.bind.annotation.*;
+
+import javax.swing.*;
+
+@RestController
+public class MvcController1 {
+
+    // @RequestMapping("/test1")
+    @GetMapping(value = "/test1")
+    public String test1() {
+        System.out.println("changed");
+        return "test1입니다.sfdgsdfgsdfg";
+    }
+    @GetMapping("/test2View")
+    public String test2() {
+        return "test2";
+    }
+
+    // 데이터 등록 메소드이다.
+    @PostMapping(value = "/postTest1")
+    public String postTEst1() {
+        return """
+                {
+                "name": "홍길동ㅂ",
+                 "age": "40"
+                }
+                """;
+    }
+
+    @PutMapping("/putTest1")
+    public String putTest1() {
+        return "put은 업데이트 http 메소드 입니다.";
+    }
+
+    @DeleteMapping("/deleteTest1")
+    public String deleteTest1() {
+        return "delete는 삭제 http 메소드 입니다.";
+    }
+}
